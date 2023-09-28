1d84c0d7022d7e1eed88eec4d0b6f4da7383cd58
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-09-28 12:54:44 +0200

    update

--- src/components/FormSubmission.tsx
+++ src/components/FormSubmission.tsx
@@ -1,6 +1,10 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { CharacterFormSchema, type CharacterFormSchemaType } from '../validations/character';
+import Button from './Button';
+import Card from './Card';
+import InputBox from './InputBox';
+import type { SubmitHandler } from 'react-hook-form';
 
 type Props = {
   handleMutate: (values: any) => any;
@@ -13,6 +17,32 @@ const FormSubmission = ({ handleMutate }: Props) => {
     reset,
     formState: { isValid, isSubmitting, errors },
   } = useForm<CharacterFormSchemaType>({
+    mode: 'all',
     resolver: zodResolver(CharacterFormSchema),
   });
+
+  const onSubmit: SubmitHandler<CharacterFormSchemaType> = async data => {
+    try {
+      await handleMutate(data);
+      reset();
+    } catch (e: any) {
+      alert(e.message);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className={'flex flex-col items-center justify-center'}>
+        <Card>
+          <InputBox label="first name" errors={errors} name="firstName" register={register} />
+          <InputBox label="last name" errors={errors} name="lastName" register={register} />
+          <InputBox label="house" errors={errors} name="house" register={register} />
+          <InputBox label="known as" errors={errors} name="knownAs" register={register} />
+          <Button disabled={!isValid} type="submit" color="primary">
+            {isSubmitting ? 'submitting..' : 'Save Character'}
+          </Button>
+        </Card>
+      </div>
+    </form>
+  );
 };
