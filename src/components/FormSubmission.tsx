1f43db63ea000e1da31e4a01f3057e1bdd4ecba3
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-09-28 12:33:38 +0200

    update

---
+++ src/components/FormSubmission.tsx
@@ -0,0 +1,9 @@
+import { useForm } from "react-hook-form";
+
+type Props = {
+  handleMutate: (values: any) => any;
+};
+
+const FormSubmission = ({handleMutate}: Props) => {
+    useForm<schemehere>()
+}
---
+++ src/validations/character.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod';
+
+export const CharacterFormSchema = z.object({
+  firstName: z.string().min(2, 'First Name must have at least 2 characters').max(20, 'Too many characters'),
+  house: z.string().nullable(),
+  knownAs: z.string().nullable(),
+  lastName: z.string().min(2, 'First Name must have at least 2 characters').max(20, 'Too many characters'),
+});
