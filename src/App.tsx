9beb2bbf27def76a7a2403c59c2def5652789f93
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-09-28 12:09:43 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { BrowserRouter } from 'react-router-dom';
@@ -33,6 +34,7 @@ function App() {
             </div>
           </ErrorBoundary>
         </div>
+        <ReactQueryDevtools initialIsOpen={false} />
       </BrowserRouter>
     </QueryClientProvider>
   );
