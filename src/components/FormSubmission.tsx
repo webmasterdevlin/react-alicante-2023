bc358ead1077c2303b9bb52d687ca6c045103865
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-09-28 12:38:03 +0200

    update

--- src/components/FormSubmission.tsx
+++ src/components/FormSubmission.tsx
@@ -1,9 +1,18 @@
-import { useForm } from "react-hook-form";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { CharacterFormSchema, type CharacterFormSchemaType } from '../validations/character';
 
 type Props = {
   handleMutate: (values: any) => any;
 };
 
-const FormSubmission = ({handleMutate}: Props) => {
-    useForm<schemehere>()
-}
+const FormSubmission = ({ handleMutate }: Props) => {
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { isValid, isSubmitting, errors },
+  } = useForm<CharacterFormSchemaType>({
+    resolver: zodResolver(CharacterFormSchema),
+  });
+};
