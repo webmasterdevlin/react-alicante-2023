6df4de7b3381d93c0f6de2281130c07ed7f4795a
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-09-28 12:02:11 +0200

    update

--- src/pages/HeroesPage.tsx
+++ src/pages/HeroesPage.tsx
@@ -1,5 +1,28 @@
+import { useQueryClient } from '@tanstack/react-query';
+import useFetchHeroes from '../features/heroes/hooks/useFetchHeroes';
+
 const HeroesPage = () => {
-  return <h1>Heroes Page</h1>;
+  const queryClient = useQueryClient();
+  const { data: response, status } = useFetchHeroes();
+  if (status === 'error') return <p>Error =( from fetching Heroes</p>;
+  return (
+    <>
+      <h1>Heroes Page</h1>
+      {status === 'loading' ? (
+        <h2>Loading.. Please wait..</h2>
+      ) : (
+        response?.data?.map(h => {
+          return (
+            <div key={h.id} className="flex items-center justify-between">
+              <h1>
+                <span>{`${h.firstName} ${h.lastName} is ${h.knownAs}`}</span>
+              </h1>
+            </div>
+          );
+        })
+      )}
+    </>
+  );
 };
 
 export default HeroesPage;
